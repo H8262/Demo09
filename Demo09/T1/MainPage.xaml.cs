9c6c90107152a83d91583ba8c6ee3f2a4496ab81
H8262 <h8262@student.jamk.fi>
2016-02-09 11:14:31 +0200

    voe

---
+++ Demo09/T1/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace T1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void textBlock1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void truckButton_Click(object sender, RoutedEventArgs e)
+        {
+            string carT = truckTextBlock.Text;
+            int car = int.Parse(carT);
+            car++;
+            carT = Convert.ToString(car);
+            truckTextBlock.Text = carT;
+        }
+
+        private void carButton_Click(object sender, RoutedEventArgs e)
+        {
+            string carT = carTextBlock.Text;
+            int car = int.Parse(carT);
+            car++;
+            carT = Convert.ToString(car);
+            carTextBlock.Text = carT;
+
+        }
+
+    }
+}
